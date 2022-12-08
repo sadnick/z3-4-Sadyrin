791c51bec45ef7d0405014fb3d4e002727c5c58c
Николай Садырин <sadyrinnk@mail.ru>
2022-12-08 20:54:17 +0300

    .

--- z3-4-Sadyrin/Pages/WarehehousePage.xaml.cs
+++ z3-4-Sadyrin/Pages/WarehehousePage.xaml.cs
@@ -58,6 +58,7 @@ namespace z3_4_Sadyrin.Pages
 
                 }
                 catch (Exception ex)
+                
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
